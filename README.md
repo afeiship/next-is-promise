f91a14d47cd5a99f7cf0685b013bfe047a7e4b15
afeiship <1290657123@qq.com>
2019-06-19 00:27:27 +0800

    add: intail

---
+++ README.md
@@ -0,0 +1,15 @@
+# next-is-promise
+> Test whether an object looks like a promisesA+ promise
+
+## installation
+```bash
+npm install -S afeiship/next-is-promise --registry=https://registry.npm.taobao.org
+```
+
+## usage
+```js
+//DOCS here!
+```
+
+## resources
+- https://github.com/then/is-promise
---
+++ src/next-is-promise.js
@@ -0,0 +1,18 @@
+(function() {
+  var global = global || this || window || Function('return this')();
+  var nx = global.nx || require('next-js-core2');
+  var OBJ = 'object';
+  var FUNC = 'function';
+
+  nx.isPromise = function(inObj) {
+    return (
+      !!inObj &&
+      (typeof inObj === OBJ || typeof inObj === FUNC) &&
+      typeof obj.then === FUNC
+    );
+  };
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = nx.isPromise;
+  }
+})();
